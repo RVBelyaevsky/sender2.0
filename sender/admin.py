1f79b33184a4bbd548ff88a35d8ff6f71f3763c5
RVBelyaevsky <rooooooma73@gmail.com>
2024-12-27 13:36:00 +0800

    sender models and migrations

--- sender/admin.py
+++ sender/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from sender.models import Client, Message, Newsletter
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'first_name', 'last_name', 'email',)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'body',)
+
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'first_sending', 'status', 'message',)
--- sender/models.py
+++ sender/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {
+    "blank": True,
+    "null": True
+}
+
+
+class Client(models.Model):
+    email = models.EmailField(verbose_name='email')
+    first_name = models.CharField(max_length=100, verbose_name='имя')
+    last_name = models.CharField(max_length=100, verbose_name='фамилия')
+    comment = models.TextField(**NULLABLE, verbose_name='комментарий')
+
+    class Meta:
+        verbose_name = 'получатель'
+        verbose_name_plural = 'получатели'
+
+    def __str__(self):
+        return f"Клиент: {self.last_name} {self.first_name}, почта {self.email}"
+
+
+class Message(models.Model):
+    title = models.CharField(max_length=150, verbose_name='тема письма')
+    body = models.TextField(verbose_name='тело письма')
+
+    class Meta:
+        verbose_name = 'сообщение'
+        verbose_name_plural = 'сообщения'
+
+    def __str__(self):
+        return self.title
+
+
+class Newsletter(models.Model):
+    STATUS_CHOICES = (
+        ('created', 'создана'),
+        ('launched', 'запущена'),
+        ('completed', 'завершена'),
+    )
+    first_sending = models.DateTimeField(verbose_name='первая отправка')
+    last_sending = models.DateTimeField(**NULLABLE, verbose_name='последняя отправка')
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, verbose_name='статус')
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, verbose_name='сообщение')
+    clients = models.ManyToManyField(Client, verbose_name='получатели')
+
+    class Meta:
+        verbose_name = 'рассылка'
+        verbose_name_plural = 'рассылки'
+
+    def __str__(self):
+        return f"{self.first_sending} {self.status}"
